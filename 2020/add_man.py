45aba727ea5c02c8c2d9313aa057c86906de5ae4
Yosafe <581515@stud.hvl.no>
2024-03-08 20:04:15 +0100

    on the way

--- 2020/add_man.py
+++ 2020/add_man.py
@@ -75,9 +75,9 @@ print(len(list_of_dicts_heurope))
 print()
 
 # list_of_dicts_h2020_title_text_3d = "3D network graph of Horizon 2020 projects and countries colaborating with Norwegian organizations",
-# list_of_dicts_h2020_title_text_2d = '2D Network Graph of Horizon 2020 Projects and Countries colaborating with Norwegian organizations'
+list_of_dicts_h2020_title_text_2d = '2D Network Graph of Horizon 2020 Projects and Countries colaborating with Norwegian organizations'
 # plot_3d(list_of_dicts_h2020, list_of_dicts_h2020_title_text_3d)
-# plot_2d(list_of_dicts_h2020, list_of_dicts_h2020_title_text_2d)
+plot_2d(list_of_dicts_h2020, list_of_dicts_h2020_title_text_2d)
 print()
 
 # list_of_dicts_heurope_title_text_3d = "3D network graph of Horizon Europe projects and countries colaborating with Norwegian organizations",
